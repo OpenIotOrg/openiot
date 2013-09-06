514b22ca3c79e01c3390b5352e3940267b0d8b30
nkef <nkef@ait.edu.gr>
2013-09-06 22:24:07 +0300

    added
    getApplication(applicationID: String): OAMO
    getService (serviceID: String): OSMO
    getAvailableAppIDs (userID: String): DescriptiveIDs
    getAvailableServiceIDs (applicationID: String): DescriptiveIDs
    
    to the sdum I/F

--- modules/sdum/sdum.core/src/main/java/org/openiot/sdum/core/rest/ServiceDeliveryUtilityManagerRsControler.java
+++ modules/sdum/sdum.core/src/main/java/org/openiot/sdum/core/rest/ServiceDeliveryUtilityManagerRsControler.java
@@ -28,6 +28,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
+import org.openiot.commons.descriptiveids.model.DescreptiveIDs;
+import org.openiot.commons.osdspec.model.OAMO;
+import org.openiot.commons.osdspec.model.OSDSpec;
+import org.openiot.commons.osdspec.model.OSMO;
 import org.openiot.commons.sdum.serviceresultset.model.SdumServiceResultSet;
 import org.openiot.sdum.core.api.impl.PollForReportImpl;
 import org.slf4j.Logger;
@@ -45,6 +49,7 @@ public class ServiceDeliveryUtilityManagerRsControler {
 	//Logger's initialization
 	final static Logger logger = LoggerFactory.getLogger(ServiceDeliveryUtilityManagerRsControler.class);
 	
+
 
 
 	/**
@@ -76,18 +81,101 @@ public class ServiceDeliveryUtilityManagerRsControler {
 	}
 	
 	
+
+	/**
+	 * Invokes a previously defined Service having the specified applicationID.
+	 * This call will produce only one Result Set.
+	 * 
+	 * @param applicationID
+	 * @return
+	 */
 	@GET
 	@Path("/pollforreport")
-	public SdumServiceResultSet pollForReport(@QueryParam("serviceID") String serviceID){
-		
-		PollForReportImpl pollForReportImpl = new PollForReportImpl(serviceID);
-		
+	public SdumServiceResultSet pollForReport(@QueryParam("applicationID") String applicationID) {
+
+		PollForReportImpl pollForReportImpl = new PollForReportImpl(applicationID);
+
 		return pollForReportImpl.getSdumServiceResultSet();
-		
+
 	}
 	
 	
 	
+	/**
+	 * Used to retrieve the description (OAMO) of an available Application.
+	 * Requires as input the Application ID
+	 * 
+	 * @param applicationID
+	 * @return
+	 */
+	@GET
+	@Path("/getApplication")
+	public OAMO getApplication(@QueryParam("applicationID") String applicationID) {
+
+		OAMO oamo = new OAMO();
+
+		return oamo;
+	}
+	
+	
+	
+	
+
+	/**
+	 * Used to retrieve the description (OSMO) of an available service. Requires
+	 * as input the Service ID
+	 * 
+	 * @param serviceID
+	 * @return
+	 */
+	@GET
+	@Path("/getService")
+	public OSMO getService(@QueryParam("serviceID") String serviceID) {
+
+		OSMO osmo = new OSMO();
+
+		return osmo;
+	}
+	
+	
+	
+	
+	/**
+	 * Used to retrieve the available applications (a list of
+	 * applicationID/ServiceName/ServiceDescription triplet) already registered
+	 * by a specific user. Requires as input the User ID.
+	 * 
+	 * @param userID
+	 * @return
+	 */
+	@GET
+	@Path("/getAvailableAppIDs")
+	public DescreptiveIDs getAvailableAppIDs(@QueryParam("userID") String userID) {
+
+		DescreptiveIDs descreptiveIDs = new DescreptiveIDs();
+
+		return descreptiveIDs;
+	}
+	
+	
+	
+
+	/**
+	 * Used to retrieve the available services (a list of
+	 * serviceID/ServiceName/ServiceDescription triplet) already registered by a
+	 * specific user. Requires as input the Service ID.
+	 * 
+	 * @param applicationID
+	 * @return
+	 */
+	@GET
+	@Path("/getAvailableServiceIDs")
+	public DescreptiveIDs getAvailableServiceIDs(@QueryParam("applicationID") String applicationID) {
+
+		DescreptiveIDs descreptiveIDs = new DescreptiveIDs();
+
+		return descreptiveIDs;
+	}
 	
 	
 
