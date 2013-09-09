0176e0c22be0d582c586692fe93f5b0abde4e124
stavrosp <spet@ait.edu.gr>
2013-09-09 11:20:40 +0300

    discover sensors method 2 implementation

---
+++ modules/scheduler/scheduler.core/src/main/java/org/openiot/scheduler/core/api/impl/DiscoverSensors/method2/SensorTypeData.java
@@ -0,0 +1,20 @@
+package org.openiot.scheduler.core.api.impl.DiscoverSensors.method2;
+
+public class SensorTypeData 
+{
+	private String label;
+	private String ID;
+	
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+}
