e6516f09607e2d15ecfe824ab4dc6f9c3437c20c
stavrosp <spet@ait.edu.gr>
2013-09-25 01:20:21 +0300

    finalized coding of UserLoginImpl

--- modules/scheduler/scheduler.core/src/main/java/org/openiot/scheduler/core/api/impl/UserLogin/UserLoginImpl.java
+++ modules/scheduler/scheduler.core/src/main/java/org/openiot/scheduler/core/api/impl/UserLogin/UserLoginImpl.java
@@ -1,5 +1,65 @@
 package org.openiot.scheduler.core.api.impl.UserLogin;
 
-public class UserLoginImpl {
+import java.util.ArrayList;
 
+import org.openiot.scheduler.core.api.impl.UserRegister.UserRegisterImpl;
+import org.openiot.scheduler.core.utils.sparql.SesameSPARQLClient;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.repository.RepositoryException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserLoginImpl 
+{
+	final static Logger logger = LoggerFactory.getLogger(UserLoginImpl.class);
+	
+	private String userMail;
+	private String userPasw;
+	
+	private String replyMessage= "";
+	
+	public UserLoginImpl(String userMail,String pasw)
+	{
+		this.userMail = userMail;
+		this.userPasw = pasw;
+				
+//		logger.debug("received: "+this.userMail);		
+
+		loginUser();
+	}
+	
+	public String getReplyMessage()
+	{	
+		return replyMessage;
+	}
+	
+	private void loginUser()
+	{
+		SesameSPARQLClient sparqlCl = null;
+		try {
+			sparqlCl = new SesameSPARQLClient();
+		} catch (RepositoryException e) {			
+			logger.error("Init sparql repository error. Error checking if mail exists",e);
+			replyMessage= "error checking if mail exists, cannot init repository";
+			return;
+		}
+		
+		//check that user entered correct email
+		TupleQueryResult qres = sparqlCl.sparqlToQResult(
+				org.openiot.scheduler.core.utils.lsmpa.entities.User.Queries.selectUserByEmail(this.userMail));
+		ArrayList<org.openiot.scheduler.core.utils.lsmpa.entities.User> usrEnt =  
+				org.openiot.scheduler.core.utils.lsmpa.entities.User.Queries.parseUserData(qres);
+		
+		if (usrEnt.size()==0)
+		{		
+			replyMessage="user mail not found";
+		}
+		else
+		{
+			if (usrEnt.get(0).getPasswd().equals(this.userPasw))
+				replyMessage=usrEnt.get(0).getId();				
+			else
+				replyMessage="wrong password";			
+		}
+	}
 }
