f83a256f48dcadd7901fc779fe278e4e9713f73b
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2014-04-24 13:52:13 +0200

    Allow registration of sensors, through the restapi, that do not have a sensorId yet. The new sensorId is added to the metadata file.

--- modules/x-gsn/src/main/java/org/openiot/gsn/http/restapi/VSManagerService.java
+++ modules/x-gsn/src/main/java/org/openiot/gsn/http/restapi/VSManagerService.java
@@ -1,26 +1,29 @@
 /**
-*    Copyright (c) 2011-2014, OpenIoT
-*   
-*    This file is part of OpenIoT.
-*
-*    OpenIoT is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU Lesser General Public License as published by
-*    the Free Software Foundation, version 3 of the License.
-*
-*    OpenIoT is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU Lesser General Public License for more details.
-*
-*    You should have received a copy of the GNU Lesser General Public License
-*    along with OpenIoT.  If not, see <http://www.gnu.org/licenses/>.
-*
-*     Contact: OpenIoT mailto: info@openiot.eu
-*     @author Jean-Paul Calbimonte
-*/
-
+ * Copyright (c) 2011-2014, OpenIoT
+ *
+ * This file is part of OpenIoT.
+ *
+ * OpenIoT is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * OpenIoT is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenIoT. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: OpenIoT mailto: info@openiot.eu
+ *
+ * @author Jean-Paul Calbimonte
+ * @author Hylke van der Schaaf
+ */
 package org.openiot.gsn.http.restapi;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigException;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileWriter;
@@ -43,96 +46,117 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigRenderOptions;
+import com.typesafe.config.ConfigValueFactory;
 
 @Path("/vsensor")
 public class VSManagerService {
-    private static final transient Logger logger = LoggerFactory.getLogger(VSManagerService.class);
+
+	private static final transient Logger logger = LoggerFactory.getLogger(VSManagerService.class);
 
 	//public VSManagerService() { super(VSManagerService.class); }
-
 	@POST
 	@Path("/{vsname}/create")
-	public Response createVS(Reader vsconfig,@PathParam("vsname") String vsname) {
-		VSensorLoader vsloader =
-				VSensorLoader.getInstance(Main.DEFAULT_VIRTUAL_SENSOR_DIRECTORY);
+	public Response createVS(Reader vsconfig, @PathParam("vsname") String vsname) {
+		VSensorLoader vsloader
+				= VSensorLoader.getInstance(Main.DEFAULT_VIRTUAL_SENSOR_DIRECTORY);
 		logger.info("Start loading vs config");
 		String xml;
-		
+
 		try {
-			xml=IOUtils.toString(vsconfig);
+			xml = IOUtils.toString(vsconfig);
 			vsconfig.close();
 		} catch (IOException e) {
-			e.printStackTrace();	
-			logger.error("Errors detected: "+e.getMessage());
-			throw new VSensorConfigException(e.getMessage());					
+			logger.error("Errors detected!", e);
+			throw new VSensorConfigException(e.getMessage(), e);
 		}
-		logger.info("The xml vs config: "+xml);
-		
+		logger.debug("The xml vs config: {}", xml);
+
 		try {
 			logger.info("Now we start");
 			vsloader.loadVirtualSensor(xml, vsname);
 			logger.info("The vs is loaded");
 		} catch (Exception e) {
-			e.printStackTrace();
-			throw new VSensorConfigException(e.getMessage());					
+			logger.error("Errors detected!", e);
+			throw new VSensorConfigException(e.getMessage(), e);
 		}
 		logger.info("Finalized loading.");
 		return Response.ok(vsname).build();
 	}
-	
+
 	@POST
 	@Path("/{vsname}/registerRdf")
-	public Response registerRdfVS(Reader metadata,@PathParam("vsname") String vsname) {
-	    SensorMetadata meta=new SensorMetadata();
-	    String filePath=VSensorLoader.getVSConfigurationFilePath(vsname).replace(".xml",".ttl");
+	public Response registerRdfVS(Reader metadata, @PathParam("vsname") String vsname) {
+		SensorMetadata meta = new SensorMetadata();
+		String filePath = VSensorLoader.getVSConfigurationFilePath(vsname).replace(".xml", ".ttl");
 		try {
-			List<String> lines =IOUtils.readLines(metadata);
-			String concat="";
-			for (String line:lines)
-				concat+=line;			
-			InputStream is=new ByteArrayInputStream(concat.getBytes());
+			List<String> lines = IOUtils.readLines(metadata);
+			String concat = "";
+			for (String line : lines) {
+				concat += line;
+			}
+			InputStream is = new ByteArrayInputStream(concat.getBytes());
 			meta.load(is);
-		    MetadataCreator.addRdfMetadatatoLSM(meta);
-			FileWriter fw = new FileWriter(filePath, true);			
-		    IOUtils.writeLines(lines, "\n", fw);
-	        fw.close();
+			MetadataCreator.addRdfMetadatatoLSM(meta);
+			FileWriter fw = new FileWriter(filePath, true);
+			IOUtils.writeLines(lines, "\n", fw);
+			fw.close();
 		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error(e.getMessage());
-			throw new VSensorConfigException("Unable to load RDF metadata for sensor. "+e.getMessage());
+			logger.error("Unable to load RDF metadata for sensor.", e);
+			throw new VSensorConfigException("Unable to load RDF metadata for sensor.", e);
 		}
 		return Response.ok().build();
-	}	
+	}
 
 	@POST
 	@Path("/{vsname}/register")
-	public Response registerVS(InputStream metadata,@PathParam("vsname") String vsname) {
-	    String sensorId;
-	    String filePath=VSensorLoader.getVSConfigurationFilePath(vsname).replace(".xml",".metadata");
+	public Response registerVS(InputStream metadata, @PathParam("vsname") String vsname) {
+		String sensorId;
+		String sensorIdOld = null;
+		String filePath = VSensorLoader.getVSConfigurationFilePath(vsname).replace(".xml", ".metadata");
 		try {
-			List<String> lines =IOUtils.readLines(metadata);
-			FileWriter fw = new FileWriter(filePath, false);			
-		    IOUtils.writeLines(lines, "\n", fw);
-	        fw.close();
-			LSMSensorMetaData lsmmd=new LSMSensorMetaData();
-			
-			lsmmd.init(ConfigFactory.parseFile(new File(filePath)));
-			sensorId=MetadataCreator.addSensorToLSM(lsmmd);
-			
+			List<String> lines = IOUtils.readLines(metadata);
+			FileWriter fw = new FileWriter(filePath, false);
+			IOUtils.writeLines(lines, "\n", fw);
+			fw.close();
+			LSMSensorMetaData lsmmd = new LSMSensorMetaData();
+
+			Config configData = ConfigFactory.parseFile(new File(filePath));
+			if (configData.hasPath("sensorID")) {
+				sensorIdOld = configData.getString("sensorID");
+			}
+			lsmmd.init(configData, true);
+			sensorId = MetadataCreator.addSensorToLSM(lsmmd);
+
+			if (sensorIdOld == null || sensorId.compareTo(sensorIdOld) != 0) {
+				logger.info("SensorId has changed from {} to {}.", sensorIdOld, sensorId);
+				lsmmd.setSensorID(sensorId);
+				Config configDataNew = configData.withValue("sensorID", ConfigValueFactory.fromAnyRef(sensorId));
+				String metadataNew = configDataNew.root().render(ConfigRenderOptions.defaults().setJson(false).setComments(false));
+				fw = new FileWriter(filePath, false);
+				IOUtils.write(metadataNew, fw);
+				fw.close();
+			}
+
 		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error(e.getMessage());
-			throw new VSensorConfigException("Unable to load RDF metadata for sensor. "+e.getMessage());
+			logger.error("Unable to load metadata for sensor", e);
+			throw new VSensorConfigException("Unable to load metadata for sensor. ", e);
 		}
 		return Response.ok(sensorId).build();
-	}	
+	}
 }
 
- class VSensorConfigException extends WebApplicationException {
+class VSensorConfigException extends WebApplicationException {
+
 	private static final long serialVersionUID = -2199585164343127464L;
 
 	public VSensorConfigException(String message) {
-        super(Response.status(Response.Status.BAD_REQUEST)
-            .entity(message).type(MediaType.TEXT_PLAIN).build());        
-    }
+		super(Response.status(Response.Status.BAD_REQUEST)
+				.entity(message).type(MediaType.TEXT_PLAIN).build());
+	}
+
+	public VSensorConfigException(String message, Throwable cause) {
+		super(cause, Response.status(Response.Status.BAD_REQUEST)
+				.entity(message).type(MediaType.TEXT_PLAIN).build());
+	}
 }
--- modules/x-gsn/src/main/java/org/openiot/gsn/metadata/LSM/LSMSensorMetaData.java
+++ modules/x-gsn/src/main/java/org/openiot/gsn/metadata/LSM/LSMSensorMetaData.java
@@ -1,24 +1,25 @@
 /**
-*    Copyright (c) 2011-2014, OpenIoT
-*   
-*    This file is part of OpenIoT.
-*
-*    OpenIoT is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU Lesser General Public License as published by
-*    the Free Software Foundation, version 3 of the License.
-*
-*    OpenIoT is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU Lesser General Public License for more details.
-*
-*    You should have received a copy of the GNU Lesser General Public License
-*    along with OpenIoT.  If not, see <http://www.gnu.org/licenses/>.
-*
-*    Contact: OpenIoT mailto: info@openiot.eu
-*    @author Sofiane Sarni
-*/
-
+ * Copyright (c) 2011-2014, OpenIoT
+ *
+ * This file is part of OpenIoT.
+ *
+ * OpenIoT is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * OpenIoT is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenIoT. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: OpenIoT mailto: info@openiot.eu
+ *
+ * @author Sofiane Sarni
+ * @author Hylke van der Schaaf
+ */
 package org.openiot.gsn.metadata.LSM;
 
 import org.openiot.gsn.utils.PropertiesReader;
@@ -33,210 +34,235 @@ import java.util.Map;
 
 public class LSMSensorMetaData {
 
-    private static final transient Logger logger = Logger.getLogger(LSMSensorMetaData.class);
-    
-    private String sensorName;
-    private String author;
-    private String sensorType;
-    private String information;
-    private String sourceType;
-    private String source;
-    private String featureOfInterest;
-    //private String[] properties;
-    //private boolean registeredToLSM;
-    private double latitude;
-    private double longitude;
-    //private String fieldNames[];
-    private Map<String, LSMFieldMetaData> fields = new HashMap<String, LSMFieldMetaData>();
-    String sensorID;
+	private static final transient Logger logger = Logger.getLogger(LSMSensorMetaData.class);
 
-    public String getSensorName() {
-        return sensorName;
-    }
+	private String sensorName;
+	private String author;
+	private String sensorType;
+	private String information;
+	private String sourceType;
+	private String source;
+	private String featureOfInterest;
+	//private String[] properties;
+	//private boolean registeredToLSM;
+	private double latitude;
+	private double longitude;
+	//private String fieldNames[];
+	private Map<String, LSMFieldMetaData> fields = new HashMap<String, LSMFieldMetaData>();
+	String sensorID;
 
-    public String getAuthor() {
-        return author;
-    }
+	public String getSensorName() {
+		return sensorName;
+	}
 
-    public String getSensorType() {
-        return sensorType;
-    }
+	public String getAuthor() {
+		return author;
+	}
 
-    public String getInformation() {
-        return information;
-    }
+	public String getSensorType() {
+		return sensorType;
+	}
 
-    public String getSourceType() {
-        return sourceType;
-    }
+	public String getInformation() {
+		return information;
+	}
 
-    public double getLatitude() {
-        return latitude;
-    }
+	public String getSourceType() {
+		return sourceType;
+	}
 
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
+	public double getLatitude() {
+		return latitude;
+	}
 
-    public double getLongitude() {
-        return longitude;
-    }
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
 
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
+	public double getLongitude() {
+		return longitude;
+	}
 
-    /*public String[] getFieldNames() {
-        return fieldNames;
-    }
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
 
-    public void setFieldNames(String[] fieldNames) {
-        this.fieldNames = fieldNames;
-    }*/
+	/*public String[] getFieldNames() {
+	 return fieldNames;
+	 }
 
-    public Map<String, LSMFieldMetaData> getFields() {
-        return fields;
-    }
-/*
-    public void setFields(Map<String, LSMFieldMetaData> fields) {
-        this.fields = fields;
-    }
-*/
-    /*public boolean isRegisteredToLSM() {
-        return registeredToLSM;
-    }*/
+	 public void setFieldNames(String[] fieldNames) {
+	 this.fieldNames = fieldNames;
+	 }*/
+	public Map<String, LSMFieldMetaData> getFields() {
+		return fields;
+	}
+	/*
+	 public void setFields(Map<String, LSMFieldMetaData> fields) {
+	 this.fields = fields;
+	 }
+	 */
+	/*public boolean isRegisteredToLSM() {
+	 return registeredToLSM;
+	 }*/
 
-    public String getSource() {
-        return source;
-    }
+	public String getSource() {
+		return source;
+	}
 
-    public void setSource(String source) {
-        this.source = source;
-    }
+	public void setSource(String source) {
+		this.source = source;
+	}
 
-    public String getSensorID() {
-        return sensorID;
-    }
+	public String getSensorID() {
+		return sensorID;
+	}
 
-    public void setSensorID(String sensorID) {
-        this.sensorID = sensorID;
-    }
+	public void setSensorID(String sensorID) {
+		this.sensorID = sensorID;
+	}
 
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("LSMSensorMetaData{")
-                .append("\nsensorName  =").append(sensorName)
-                .append("\nauthor      =").append(author)
-                .append("\nsensorType  =").append(sensorType)
-                .append("\ninformation =").append(information)
-                .append("\nsourceType  =").append(sourceType)
-                .append("\nsource      =").append(source)
-                .append("\nsensorID    =").append(sensorID)
-                .append("\nfields =>\n");
-        for (String fieldName:fields.keySet()) {
-            sb.append("\t").append(fieldName).append(" : ").append(fields.get(fieldName)).append("\n");
-        }
-        return sb.toString();
-    }
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("LSMSensorMetaData{")
+				.append("\nsensorName  =").append(sensorName)
+				.append("\nauthor      =").append(author)
+				.append("\nsensorType  =").append(sensorType)
+				.append("\ninformation =").append(information)
+				.append("\nsourceType  =").append(sourceType)
+				.append("\nsource      =").append(source)
+				.append("\nsensorID    =").append(sensorID)
+				.append("\nfields =>\n");
+		for (String fieldName : fields.keySet()) {
+			sb.append("\t").append(fieldName).append(" : ").append(fields.get(fieldName)).append("\n");
+		}
+		return sb.toString();
+	}
 
-    public void setSensorName(String sensorName) {
-        this.sensorName = sensorName;
-    }
+	public void setSensorName(String sensorName) {
+		this.sensorName = sensorName;
+	}
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
-    public void setSensorType(String sensorType) {
-        this.sensorType = sensorType;
-    }
+	public void setSensorType(String sensorType) {
+		this.sensorType = sensorType;
+	}
 
-    public void setInformation(String information) {
-        this.information = information;
-    }
+	public void setInformation(String information) {
+		this.information = information;
+	}
 
-    public void setSourceType(String sourceType) {
-        this.sourceType = sourceType;
-    }
+	public void setSourceType(String sourceType) {
+		this.sourceType = sourceType;
+	}
 
-    public void init(Config conf){
-        logger.info("tripy "+conf.getString("author"));
-        
-    	this.setSensorName(conf.getString("sensorName"));
-        this.setAuthor(conf.getString("author"));
-        this.setInformation(conf.getString("information"));
-        this.setSensorType(conf.getString("sensorType"));
-        this.setSourceType(conf.getString("sourceType"));
-        this.setSource(conf.getString("source"));
-        this.setSensorID(conf.getString("sensorID"));
-        this.setLatitude(conf.getDouble("latitude"));
-        this.setLongitude(conf.getDouble("longitude"));
+	public void init(Config conf) {
+		init(conf, false);
+	}
 
-        /*String registeredToLSMString = PropertiesReader.readProperty(fileName, "registered");
-        if (registeredToLSMString.equalsIgnoreCase("true"))
-            registeredToLSM = true;
-        else
-            registeredToLSM = false;*/
-        String listOfFieldsString = conf.getString("fields");
-        String [] fieldNames = listOfFieldsString.trim().split(",");
-        for (int i = 0; i < fieldNames.length; i++) {
-            String fieldName = fieldNames[i];
-            logger.info(i + " : " + fieldName);
-            LSMFieldMetaData lsmFieldMetaData = new LSMFieldMetaData();
-            lsmFieldMetaData.setGsnFieldName(fieldName);
-            lsmFieldMetaData.setLsmPropertyName(conf.getString("field." + fieldName + "." + "propertyName"));
-            lsmFieldMetaData.setLsmUnit(conf.getString("field." + fieldName + "." + "unit"));
-            fields.put(fieldName, lsmFieldMetaData);
-            logger.info(fields.get(fieldName));
-        }
-        /*String [] props = new String[fieldNames.length];
-        int i=0;
-        for (LSMFieldMetaData field : fields.values()){
-        	props[i]=field.getLsmPropertyName();i++;
-        }
-        this.setProperties(props);*/
+	/**
+	 * Initialize the sensor metadata from the given configuration set. If the
+	 * data set does not contain a sensorId, and allowMissingSensorId is not
+	 * true, an exception will be thrown. If allowMissingSensorId is true, then
+	 * the sensorId should be set externally.
+	 *
+	 * @param conf The configuration to use for initailization.
+	 * @param allowMissingSensorId Flag stating whether a missing sensorId is
+	 * allowed.
+	 */
+	public void init(Config conf, boolean allowMissingSensorId) {
+		logger.info("tripy " + conf.getString("author"));
 
-    /*} catch (NullPointerException e) {
-        logger.warn("Error while reading properties file: " + fileName);
-        logger.warn(e);
-        return false;
-    }*/
+		this.setSensorName(conf.getString("sensorName"));
+		this.setAuthor(conf.getString("author"));
+		this.setInformation(conf.getString("information"));
+		this.setSensorType(conf.getString("sensorType"));
+		this.setSourceType(conf.getString("sourceType"));
+		this.setSource(conf.getString("source"));
 
-    //return true;
+		if (conf.hasPath("sensorID")) {
+			this.setSensorID(conf.getString("sensorID"));
+		} else {
+			if (!allowMissingSensorId) {
+				// This will throw an exception.
+				conf.getString("sensorID");
+			}
+		}
 
-    }
-    
-    public void initFromConfigFile(String fileName) {
+		this.setLatitude(conf.getDouble("latitude"));
+		this.setLongitude(conf.getDouble("longitude"));
 
-        //try {
-            //TODO: optimization: read properties file once, then scan for each field
-            logger.debug("Read file "+fileName);
-            
-            Config conf = ConfigFactory.parseFile(new File(fileName));
-            init(conf);
-            }
+		/*String registeredToLSMString = PropertiesReader.readProperty(fileName, "registered");
+		 if (registeredToLSMString.equalsIgnoreCase("true"))
+		 registeredToLSM = true;
+		 else
+		 registeredToLSM = false;*/
+		String listOfFieldsString = conf.getString("fields");
+		String[] fieldNames = listOfFieldsString.trim().split(",");
+		for (int i = 0; i < fieldNames.length; i++) {
+			String fieldName = fieldNames[i];
+			logger.info(i + " : " + fieldName);
+			LSMFieldMetaData lsmFieldMetaData = new LSMFieldMetaData();
+			lsmFieldMetaData.setGsnFieldName(fieldName);
+			lsmFieldMetaData.setLsmPropertyName(conf.getString("field." + fieldName + "." + "propertyName"));
+			lsmFieldMetaData.setLsmUnit(conf.getString("field." + fieldName + "." + "unit"));
+			fields.put(fieldName, lsmFieldMetaData);
+			logger.info(fields.get(fieldName));
+		}
+		/*
+		 String [] props = new String[fieldNames.length];
+		 int i=0;
+		 for (LSMFieldMetaData field : fields.values()){
+		 props[i]=field.getLsmPropertyName();i++;
+		 }
+		 this.setProperties(props);*/
 
-    public boolean updateSensorIDInConfigFile(String fileName, String sensorID) {
-        return PropertiesReader.writeProperty(fileName, "sensorID", sensorID);
-    }
+		/*
+		 } catch (NullPointerException e) {
+		 logger.warn("Error while reading properties file: " + fileName);
+		 logger.warn(e);
+		 return false;
+		 }*/
+		//return true;
+	}
 
-    public boolean setSensorAsRegistered(String fileName) {
-        return PropertiesReader.writeProperty(fileName, "registered", "true");
-    }
+	public void initFromConfigFile(String fileName) {
+
+		//try {
+		//TODO: optimization: read properties file once, then scan for each field
+		logger.debug("Read file " + fileName);
+
+		Config conf = ConfigFactory.parseFile(new File(fileName));
+		init(conf);
+	}
+
+	public boolean updateSensorIDInConfigFile(String fileName, String sensorID) {
+		return PropertiesReader.writeProperty(fileName, "sensorID", sensorID);
+	}
+
+	public boolean setSensorAsRegistered(String fileName) {
+		return PropertiesReader.writeProperty(fileName, "registered", "true");
+	}
 
 	public String[] getProperties() {
-		String [] props = new String[fields.size()];
-        int i=0;
-        for (LSMFieldMetaData field : fields.values()){
-        	props[i]=field.getLsmPropertyName();i++;
-        }
+		String[] props = new String[fields.size()];
+		int i = 0;
+		for (LSMFieldMetaData field : fields.values()) {
+			props[i] = field.getLsmPropertyName();
+			i++;
+		}
 		return props;
 	}
-/*
-	public void setProperties(String[] properties) {
-		this.properties = properties;
-	}
-*/
+	/*
+	 public void setProperties(String[] properties) {
+	 this.properties = properties;
+	 }
+	 */
+
 	public String getFeatureOfInterest() {
 		return featureOfInterest;
 	}
