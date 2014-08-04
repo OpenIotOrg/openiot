7cb9ba3e2dca79e870e734ea2a8cfa4736666d74
jpcik <jean.paul.ik@gmail.com>
2014-08-04 23:09:14 +0200

    example of generated rdf metadata

---
+++ modules/x-gsn/src/test/java/org/openiot/gsn/metadata/rdf/RdfMetadataTest.java
@@ -0,0 +1,25 @@
+package org.openiot.gsn.metadata.rdf;
+
+import org.openiot.gsn.metadata.LSM.LSMFieldMetaData;
+import org.openiot.gsn.metadata.LSM.LSMSensorMetaData;
+
+public class RdfMetadataTest {
+   public static void main(String[] args){
+	   SensorMetadata sm= new SensorMetadata();
+	   LSMSensorMetaData meta = new LSMSensorMetaData();
+	   meta.setSensorID("http://example.org/dig/gogo#ffsdfsdf");
+	   meta.setFeatureOfInterest("http://example.org/feature");
+	   meta.setSensorName("Name of Sensor");
+	   meta.setSourceType("sourceType1");
+	   meta.setAuthor("The Author");
+	   meta.setSensorType("NewType");
+	   meta.setLatitude(20);
+	   meta.setLongitude(30);
+	   LSMFieldMetaData f=new LSMFieldMetaData();
+	   f.setLsmPropertyName("http://example.org/propTemp");
+	   f.setLsmUnit("celsius");
+	   meta.getFields().put("temp", f);
+	   
+	   sm.createMetadata(meta );
+   }
+}
