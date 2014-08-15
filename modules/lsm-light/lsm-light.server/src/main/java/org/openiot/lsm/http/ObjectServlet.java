a3f1c06cc4909338365f4c8d49fd3316a8a02f3f
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2014-08-15 11:14:19 +0200

    * Fixed missing .toString() on passed objects.

--- modules/lsm-light/lsm-light.server/src/main/java/org/openiot/lsm/http/ObjectServlet.java
+++ modules/lsm-light/lsm-light.server/src/main/java/org/openiot/lsm/http/ObjectServlet.java
@@ -2,7 +2,7 @@ package org.openiot.lsm.http;
 
 /**
 *    Copyright (c) 2011-2014, OpenIoT
-*   
+*
 *    This file is part of OpenIoT.
 *
 *    OpenIoT is free software: you can redistribute it and/or modify
@@ -51,15 +51,15 @@ import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 /**
- * 
+ *
  * @author Hoan Nguyen Mau Quoc
- * 
+ *
  */
 
 @WebServlet("/ObjectServlet")
 public class ObjectServlet extends HttpServlet {
 //	private static final long serialVersionUID = 2L;
-       
+
 	final static Logger logger = LoggerFactory.getLogger(ObjectServlet.class);
     static private PropertyManagement propertyManagement = null;
     /**
@@ -72,10 +72,10 @@ public class ObjectServlet extends HttpServlet {
 
 	public void init(ServletConfig config) throws ServletException {
 	    super.init(config);
-	    ConstantsUtil.realPath = this.getServletContext().getRealPath("WEB-INF");	   
+	    ConstantsUtil.realPath = this.getServletContext().getRealPath("WEB-INF");
 	    propertyManagement = new PropertyManagement();
 	}
-	  
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -89,70 +89,70 @@ public class ObjectServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String sb = "";  
-				
-	    PrintWriter out= response.getWriter();  
-		try { 			
+		String sb = "";
+
+	    PrintWriter out= response.getWriter();
+		try {
 			ObjectInputStream inputFromClient = new ObjectInputStream(request.getInputStream());
 			// deserialize the object, note the cast
-			Object object = inputFromClient.readObject();			
+			Object object = inputFromClient.readObject();
 			String graphURL = request.getHeader("graphURL");
-	        String api = request.getHeader("api");	      
+	        String api = request.getHeader("api");
 	        String apiType = request.getHeader("apiType");
-//	        System.out.println(api); 
+//	        System.out.println(api);
 	        String token = request.getHeader("token");
 	        String clientId = request.getHeader("clientId");
 	        logger.info("API function:"+api);
-	       
-	        if(NumberUtil.isInteger(api)){        
+
+	        if(NumberUtil.isInteger(api)){
 		      	sb = processRequestImpl(api,object,clientId,token);
-		    }	
+		    }
         	response.setContentType("text/xml");
             response.setHeader("Pragma", "no-cache"); // HTTP 1.0
             response.setHeader("Cache-Control", "no-cache"); // HTTP 1.1
-	        out.println(sb);  
-	        out.close();  
+	        out.println(sb);
+	        out.close();
 	        logger.info(sb);
-        } catch (Exception ex) {  
-            out.println(sb);  
+        } catch (Exception ex) {
+            out.println(sb);
 //            ex.printStackTrace();
-            out.close();  
+            out.close();
             logger.error("Server returns error",ex);
-        } 
+        }
 	}
 
 
 	private String processRequestImpl(String api, Object object,String clientId,String token) {
 		// TODO Auto-generated method stub
-		String result="Your request processed successfully";		
+		String result="Your request processed successfully";
 		try {
 			 SensorManager sensorManager = new SensorManager();
-			 Sensor sensor = null;	
+			 Sensor sensor = null;
 			 String triples = "";
 			 switch(api){
-	        	case "21":	        		
+	        	case "21":
 					if(object instanceof Sensor)
 						sensor = (Sensor) object;
-					else break;						
-					logger.info("add new sensor with id = "+sensor.getId()); 
-					
+					else break;
+					logger.info("add new sensor with id = "+sensor.getId());
+
 					if((sensor.getMetaGraph()==null)||(sensor.getMetaGraph()==""))
 	        			sensor.setMetaGraph(propertyManagement.getSchedulerLsmMetaGraph());
-					
+
 					String permissionString = "";
 					if(PermissionsUtil.getUserType(sensor.getMetaGraph())==PermissionsUtil.GUESS_USER)
 						permissionString = PermissionsUtil.ADD_SENSOR_GUESS;
 					else if(PermissionsUtil.getUserType(sensor.getMetaGraph())==PermissionsUtil.DEMO_USER)
 						permissionString = PermissionsUtil.ADD_SENSOR_DEMO;
 					else permissionString = PermissionsUtil.ADD_SENSOR_MAIN;
-					
+
 					if(SecurityUtil.hasPermission(PermissionsUtil.LSM_ALL, getServletContext(), token, clientId)
-						||SecurityUtil.hasPermission(permissionString, getServletContext(), token, clientId)){						
+						||SecurityUtil.hasPermission(permissionString, getServletContext(), token, clientId)){
 		        		sensorManager.setDataGraph(sensor.getDataGraph());
 		        		sensorManager.setMetaGraph(sensor.getMetaGraph());
 //				        String sensorTypeId = sensorManager.getSensorTypeId(sensor.getSensorType().toLowerCase());
 		        		triples = TriplesDataRetriever.getSensorTripleMetadata(sensor);
-//		        		logger.info(triples);		        		
+//		        		logger.info(triples);
 		        		sensorManager.insertTriplesToGraph(sensor.getMetaGraph(), triples);
 //		        		sensorManager.runSpatialIndex();
 		        		logger.info("Add new sensor");
@@ -166,23 +166,23 @@ public class ObjectServlet extends HttpServlet {
 	        		Observation observation = null;
 					if(object instanceof Observation)
 						observation = (Observation) object;
-					else break;						
+					else break;
 				    logger.info("add Observation object with id = "+observation.getId());
 	        		triples = "";
 	        		if((observation.getMetaGraph()==null)||(observation.getMetaGraph()==""))
-	        			observation.setMetaGraph(propertyManagement.getLSMLocalMetaGraph());	        		
-	        		
+	        			observation.setMetaGraph(propertyManagement.getLSMLocalMetaGraph());
+
 	        		if(PermissionsUtil.getUserType(observation.getMetaGraph())==PermissionsUtil.GUESS_USER)
 	        			permissionString = PermissionsUtil.UPDATE_SENSOR_DATA_GUESS;
 	        		else if(PermissionsUtil.getUserType(observation.getMetaGraph())==PermissionsUtil.DEMO_USER)
 	        			permissionString = PermissionsUtil.UPDATE_SENSOR_DATA_DEMO;
 	        		else permissionString = PermissionsUtil.UPDATE_SENSOR_DATA_MAIN;
-	        		
+
 	        		if(SecurityUtil.hasPermission(PermissionsUtil.LSM_ALL, getServletContext(), token, clientId)
 							||SecurityUtil.hasPermission(permissionString, getServletContext(), token, clientId)){
 		        		sensorManager.setDataGraph(observation.getDataGraph());
 		        		sensorManager.setMetaGraph(observation.getMetaGraph());
-	
+
 		        		sensor = sensorManager.getSpecifiedSensorWithSensorId(observation.getSensor());
 		        		if(sensor==null){
 		        			result="Sensor "+observation.getSensor()+" has not been registered yet. Please register your sensor!";
@@ -190,22 +190,22 @@ public class ObjectServlet extends HttpServlet {
 		        		}
 		        		String foi = "";
 		        		if(observation.getFeatureOfInterest().equals("")||observation.getFeatureOfInterest()==null)
-		        			foi = ConnectionManager.propertyManagement.getOpeniotResourceNamespace() + 
+		        			foi = ConnectionManager.propertyManagement.getOpeniotResourceNamespace() +
 								Double.toString(sensor.getPlace().getLat()).replace(".", "").replace("-", "")+
-								Double.toString(sensor.getPlace().getLng()).replace(".", "").replace("-", "");	
-		        		else foi = observation.getFeatureOfInterest();					
+								Double.toString(sensor.getPlace().getLng()).replace(".", "").replace("-", "");
+		        		else foi = observation.getFeatureOfInterest();
 		        		triples+=TriplesDataRetriever.getObservationTripleData(observation.getId(), observation.getSensor(), foi, observation.getTimes());
-		        		
-		        		OntModel model = ModelFactory.createOntologyModel();	        		        		
+
+		        		OntModel model = ModelFactory.createOntologyModel();
 		        		for(ObservedProperty obv : observation.getReadings()){
-		        			OntClass cl = model.createClass(obv.getPropertyType());	
+		        			OntClass cl = model.createClass(obv.getPropertyType());
 		        			if(obv.getUnit().equals(""))
-		        				triples+=TriplesDataRetriever.getTripleDataHasNoUnit("http://purl.oclc.org/NET/ssnx/ssn#ObservationValue",cl.getLocalName(),obv.getValue(), 
+		        				triples+=TriplesDataRetriever.getTripleDataHasNoUnit("http://purl.oclc.org/NET/ssnx/ssn#ObservationValue",cl.getLocalName(),obv.getValue().toString(),
 		        						observation.getId(),sensor.getProperties().get(obv.getPropertyType()), observation.getTimes());
-		        			else triples+=TriplesDataRetriever.getTripleDataHasUnit("http://purl.oclc.org/NET/ssnx/ssn#ObservationValue",cl.getLocalName(),obv.getValue(),obv.getUnit(),
+		        			else triples+=TriplesDataRetriever.getTripleDataHasUnit("http://purl.oclc.org/NET/ssnx/ssn#ObservationValue",cl.getLocalName(),obv.getValue().toString(),obv.getUnit(),
 		        						observation.getId(),sensor.getProperties().get(obv.getPropertyType()), observation.getTimes());
 		        		}
-	//	        		System.out.println(triples);	 
+	//	        		System.out.println(triples);
 		        		sensorManager.insertTriplesToGraph(observation.getDataGraph(), triples);
 		        		logger.info("Add new sensor data successfully");
 	        		}else{
@@ -213,19 +213,19 @@ public class ObjectServlet extends HttpServlet {
 			 			logger.info(result);
 					}
 	        		break;
-	        	case "23":	        		
+	        	case "23":
 	        		RDFTuple tuple = null;
 					if(object instanceof RDFTuple)
 						tuple = (RDFTuple) object;
-					else break;						
-//				    System.out.println(tuple.getNtriple()); 					
-					
+					else break;
+//				    System.out.println(tuple.getNtriple());
+
 					if(PermissionsUtil.getUserType(tuple.getGraphURL())==PermissionsUtil.GUESS_USER)
 						permissionString = PermissionsUtil.ADD_TRIPLES_GUESS;
 					else if(PermissionsUtil.getUserType(tuple.getGraphURL())==PermissionsUtil.DEMO_USER)
 						permissionString = PermissionsUtil.ADD_TRIPLES_DEMO;
 					else permissionString = PermissionsUtil.ADD_TRIPLES_MAIN;
-						
+
 					if(SecurityUtil.hasPermission(PermissionsUtil.LSM_ALL, getServletContext(), token, clientId)
 							||SecurityUtil.hasPermission(permissionString, getServletContext(), token, clientId)){
 		        		sensorManager.insertTriplesToGraph(tuple.getGraphURL(), tuple.getNtriple());
@@ -235,18 +235,18 @@ public class ObjectServlet extends HttpServlet {
 			 			logger.info(result);
 					}
 	        		break;
-	        	case "24":	        		
+	        	case "24":
 	        		tuple = null;
 					if(object instanceof RDFTuple)
 						tuple = (RDFTuple) object;
-					else break;						
-//				    System.out.println(tuple.getNtriple()); 
+					else break;
+//				    System.out.println(tuple.getNtriple());
 					if(PermissionsUtil.getUserType(tuple.getGraphURL())==PermissionsUtil.GUESS_USER)
 						permissionString = PermissionsUtil.DEL_TRIPLES_GUESS;
 					else if(PermissionsUtil.getUserType(tuple.getGraphURL())==PermissionsUtil.DEMO_USER)
 						permissionString = PermissionsUtil.DEL_TRIPLES_DEMO;
 					else permissionString = PermissionsUtil.DEL_TRIPLES_MAIN;
-					
+
 					if(SecurityUtil.hasPermission(PermissionsUtil.LSM_ALL, getServletContext(), token, clientId)
 							||SecurityUtil.hasPermission(permissionString, getServletContext(), token, clientId)){
 						if(tuple.getNtriple().equals("all")){
@@ -260,16 +260,16 @@ public class ObjectServlet extends HttpServlet {
 			 			result ="You don't have permmison to operate this funtion";
 			 			logger.info(result);
 					}
-	        		break;					
+	        		break;
 	        	case "25":
-	        		HashMap<String, String> patterns = null;	        		
-	        		
+	        		HashMap<String, String> patterns = null;
+
 	        		if(PermissionsUtil.getUserType(patterns.get("graph"))==PermissionsUtil.GUESS_USER)
 	        			permissionString = PermissionsUtil.UPDATE_TRIPLES_GUESS;
 					else if(PermissionsUtil.getUserType(patterns.get("graph"))==PermissionsUtil.DEMO_USER)
 						permissionString = PermissionsUtil.UPDATE_TRIPLES_DEMO;
 					else permissionString = PermissionsUtil.UPDATE_TRIPLES_MAIN;
-	        		
+
 	        		if(SecurityUtil.hasPermission(PermissionsUtil.LSM_ALL, getServletContext(), token, clientId)
 							||SecurityUtil.hasPermission(permissionString, getServletContext(), token, clientId)){
 		        		if(object instanceof HashMap<?,?>)
@@ -281,10 +281,10 @@ public class ObjectServlet extends HttpServlet {
 					}
 	        	default:
 	        		break;
-			 }		    			 
-			 			  
+			 }
+
 		} catch (Exception e) {
-//			e.printStackTrace();	
+//			e.printStackTrace();
 			logger.error("Server returns error",e);
 			result = e.toString();
 		}
